a0d80bb17645d81eb73e6b1407e66baaf2bcb3e3
Squall7218 <Squall7218@gmail.com>
2018-02-17 20:50:03 -0200

    Initial commit

---
+++ src/main/java/automaton/AutomatonMod.java
@@ -0,0 +1,18 @@
+package automaton;
+
+import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.Mod.EventHandler;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
+
+@Mod(modid = AutomatonMod.MODID, version = AutomatonMod.VERSION)
+public class AutomatonMod
+{
+    public static final String MODID = "Automaton";
+    public static final String VERSION = "1.0";
+    
+    @EventHandler
+    public void init(FMLInitializationEvent event)
+    {
+
+    }
+}
